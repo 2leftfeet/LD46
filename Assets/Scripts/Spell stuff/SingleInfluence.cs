ca856f7b94ffc0b3edeb41a0c943dee3738a4489
Liutauras Gaidamavičius <liutauras@unity3d.com>
2020-04-18 20:30:40 +0300

    Sacrificing works

--- Assets/Scripts/Spell stuff/SingleInfluence.cs
+++ Assets/Scripts/Spell stuff/SingleInfluence.cs
@@ -14,14 +14,31 @@ public class SingleInfluence : ISpell
         if(hit.collider != null)
         {
             var villager = hit.collider.GetComponent<VillagerAI>();
+            Debug.Log("yabba");
             if(villager)
             {
-                villager.Possess(caster.transform);
-
-                var possessedVillagers = caster.GetComponent<PossessedVillagers>().possessedVillagers;
-                if(!possessedVillagers.Contains(villager))
-                    possessedVillagers.Add(villager);
+                Possess(caster, villager);
+                Debug.Log("yabba");
             }
+            else
+            {
+                villager = hit.collider.GetComponentInParent<VillagerAI>();
+                Debug.Log("yabba");
+                if(villager)
+                    Possess(caster, villager);
+            }
+
+
+            
         }
+    }
+
+    void Possess(Caster caster, VillagerAI villager)
+    {
+        villager.Possess(caster.transform);
+
+        var possessedVillagers = caster.GetComponent<PossessedVillagers>().possessedVillagers;
+        if(!possessedVillagers.Contains(villager))
+            possessedVillagers.Add(villager);
     }
 }
