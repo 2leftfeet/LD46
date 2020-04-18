0dfc84b52e5722ae68c2cb170949373c2ad7c345
pauliusg <pauliusg@unity3d.com>
2020-04-18 12:00:01 +0300

    Some foundation for spells

---
+++ Assets/Scripts/Misc/CursorManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorManager : MonoBehaviour
+{
+    public static CursorManager instance;
+
+    private enum CursorStates
+    {
+        DEFAULT,
+        CAST
+    }
+    private CursorStates state;
+    [SerializeField] private Sprite defaultCursor = default;
+    [SerializeField] private Sprite castCursor = default;
+
+    private RectTransform cursorRect;
+    private Image cursorImage;
+
+    private Vector2 defaultPivot;
+    private Vector2 castPivot;
+
+    private void Awake()
+    {
+        instance = this;
+        cursorRect = transform.GetComponent<RectTransform>();
+        cursorImage = transform.GetComponent<Image>();
+        state = CursorStates.DEFAULT;
+
+        defaultPivot = new Vector2(0.12f, 0.88f);
+        castPivot = new Vector2(0.5f, 0.5f);
+    }
+
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        // No idea why I have to always hide it but it doesn't work only on start.
+        Cursor.visible = false;
+        Vector3 mousePos = Input.mousePosition;
+        cursorRect.position = mousePos;
+    }
+
+    public void SetCursorState(string which)
+    {
+        if(which == "CAST")
+        {
+            state = CursorStates.CAST;
+            cursorImage.sprite = castCursor;
+            cursorRect.pivot = castPivot;
+        } 
+        else if(which == "DEFAULT")
+        {
+            state = CursorStates.DEFAULT;
+            cursorImage.sprite = defaultCursor;
+            cursorRect.pivot = defaultPivot;
+        }
+    }
+}
---
+++ Assets/Scripts/Spell stuff/Caster.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caster : MonoBehaviour
+{
+    [Header("Keycode for casting single influence")]
+    public KeyCode singleKey = default;
+    [Header("Keycode for cancelling cast")]
+    public KeyCode cancelKey = default;
+    
+}
