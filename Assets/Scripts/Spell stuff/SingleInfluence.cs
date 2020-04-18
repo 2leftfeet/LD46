dfb69fa46bd5a32f01ffa09e416e372a4ed1813d
Justas <justasz@unity3d.com>
2020-04-18 18:11:48 +0300

    Update from master

--- Assets/Scripts/Spell stuff/SingleInfluence.cs
+++ Assets/Scripts/Spell stuff/SingleInfluence.cs
@@ -2,58 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SingleInfluence : MonoBehaviour
+public class SingleInfluence : ISpell
 {
-    [SerializeField] private float range = default;
 
-    private Caster thisCaster;
-    private KeyCode singleKey;
-    private KeyCode cancelKey;
-
-    private void Awake()
-    {
-        thisCaster = transform.GetComponent<Caster>();
-        singleKey = thisCaster.singleKey;
-        cancelKey = thisCaster.cancelKey;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Target();
-        Cast();
-    }
-
-    private void Target()
-    {
-        // Enter targeting state
-        if(Input.GetKeyDown(singleKey) && !thisCaster.isTargeting)
-            thisCaster.StartCast(range);
-
-        // Exit targeting state
-        else if(Input.GetKeyDown(cancelKey) && thisCaster.isTargeting)
-            thisCaster.EndCast();
-
-    }
 
     // Will have to check for more shit once it's an actual spell
-    private void Cast()
+    public void Cast()
     {
-        if(thisCaster.isTargeting && Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Vector2 casterPos = transform.position;
-            Vector2 targetPos = CursorManager.instance.GetWorldSpacePosition();
-
-            if(thisCaster.IsDistanceValid(casterPos, targetPos, range))
-            {
-                thisCaster.EndCast();
-                Influence();
-            }
-        }
+        Influence();
     }
 
     private void Influence()
     {
-        // Dunno the logic here.
+        Debug.Log("I did single influence");
     }
 }
---
+++ Assets/Scripts/Spell stuff/SpellObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpellType
+{
+    SingleInfluence
+}
+
+[CreateAssetMenu(menuName="Spells/Spell")]
+public class SpellObject : ScriptableObject
+{
+    public float range;
+    public KeyCode castKey;
+    public SpellType type;
+    ISpell spellLogic;
+
+    void OnEnable()
+    {
+        switch(type)
+        {
+            case SpellType.SingleInfluence:
+                spellLogic = new SingleInfluence();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Cast()
+    {
+        spellLogic.Cast();
+    }
+}
