a272205354dbb5144781e5664b0e5a087a24fb2c
Liutauras Gaidamavičius <liutauras@unity3d.com>
2020-04-19 17:34:45 +0300

    Skin tone generation

--- Assets/Scripts/RandomizeColor.cs
+++ Assets/Scripts/RandomizeColor.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RandomizeColor : MonoBehaviour
 {
+    public Vector3 skinMinHSV;
+    public Vector3 skinMaxHSV;
+
     public float minSaturation;
     public float maxSaturation;
 
@@ -17,5 +20,11 @@ public class RandomizeColor : MonoBehaviour
         var randomColor2 = Random.ColorHSV(0, 1, minSaturation, maxSaturation, minLightness, maxLightness);
         renderer.material.SetColor("_Accent1Changed", randomColor1);
         renderer.material.SetColor("_Accent2Changed", randomColor2);
+
+        skinMinHSV /= 255;
+        skinMaxHSV /= 255;
+
+        var randomSkin = Random.ColorHSV(skinMinHSV.x, skinMaxHSV.x, skinMinHSV.y, skinMaxHSV.y, skinMinHSV.z, skinMaxHSV.z);
+        renderer.material.SetColor("_SkinChanged", randomSkin);
     }
 }
