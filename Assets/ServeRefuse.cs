1d3929099f1223d111615acd259d94fb725523ff
Rokevicius <rokas@kazlauskas.eu>
2020-04-20 16:29:54 +0300

    Intro scene v1

--- Assets/ServeRefuse.cs
+++ Assets/ServeRefuse.cs
@@ -7,18 +7,22 @@ using TMPro;
  public class ServeRefuse: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
  {
     [SerializeField] TextMeshProUGUI textmeshPro;
-    [SerializeField] GameObject demonGood;
+    [SerializeField] GameObject demonGoodText;
+    [SerializeField] GameObject demonBadText;
+
     [SerializeField] GameObject demonBad;
     public void OnPointerEnter(PointerEventData eventData)
     {
         textmeshPro.text = "SERVE";
-        demonGood.SetActive(false);
+        demonBadText.SetActive(true);
+        demonGoodText.SetActive(false);
         demonBad.SetActive(true);
     }
     public void OnPointerExit(PointerEventData eventData)
     {
         textmeshPro.text = "REFUSE";
-        demonGood.SetActive(true);
+        demonGoodText.SetActive(true);
+        demonBadText.SetActive(false);
         demonBad.SetActive(false);
     }
 }
