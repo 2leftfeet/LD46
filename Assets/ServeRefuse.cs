735494987710c4819f0f9e3103d0bc2449ab46d7
Rokevicius <rokas@kazlauskas.eu>
2020-04-20 15:51:00 +0300

    Intro

---
+++ Assets/ServeRefuse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+ 
+ public class ServeRefuse: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+ {
+    [SerializeField] TextMeshProUGUI textmeshPro;
+    [SerializeField] GameObject demonGood;
+    [SerializeField] GameObject demonBad;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        textmeshPro.text = "SERVE";
+        demonGood.SetActive(false);
+        demonBad.SetActive(true);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        textmeshPro.text = "REFUSE";
+        demonGood.SetActive(true);
+        demonBad.SetActive(false);
+    }
+}
+
